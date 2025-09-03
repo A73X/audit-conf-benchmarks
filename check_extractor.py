81b1f29e8bb8f2dda9f2fea4ed7ea9b2091c1df6
MatthisClavijo <matthis.clavijo@gmail.com>
2025-09-03 16:47:40 +0200

    Added check for value extraction error

--- check_extractor.py
+++ check_extractor.py
@@ -112,6 +112,8 @@ class CheckExtractor:
                         # Handle normal case (same value for all keys)
                         for regkey in regkeys_l:
                             self.checks_values_d[regkey] = reg_info
+            else:
+                self.checks_values_d[regkey] = None
 
     def __parse_registry_line(self, line):
         """
